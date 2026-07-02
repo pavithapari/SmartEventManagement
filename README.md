624431dddfe901524a7cfdfcad56eb10f9c7f10c
Pavitha P <pavithapari10@gmail.com>
2026-07-03 00:04:50 +0530

    Created the migrations files added the views for the account and created basic template then added css for all usage

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # SmartEventManagement
 This is the project that we build for the siteCraft competion
+
+event_portal/
+│
+├── accounts/
+├── events/
+├── registrations/
+│
+├── templates/
+│   ├── accounts/
+│   ├── events/
+│   └── registrations/
+│
+├── static/
+│   ├── css/
+│   ├── js/
+│   └── images/
+│
+├── media/
+│   ├── event_banners/
+│   └── profile_pictures/
+│
+├── event_portal/
+│
+├── manage.py
+├── requirements.txt
+└── .gitignore
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm, ProfileForm
 
-# Create your views here.
+
+def register(request):
+
+    if request.method == "POST":
+
+        user_form = UserRegistrationForm(request.POST)
+        profile_form = ProfileForm(request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+
+            user = user_form.save(commit=False)
+
+            user.set_password(user_form.cleaned_data["password"])
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+
+            return redirect("login")
+
+    else:
+        user_form = UserRegistrationForm()
+        profile_form = ProfileForm()
+
+    context = {
+        "user_form": user_form,
+        "profile_form": profile_form,
+    }
+
+    return render(request, "accounts/register.html", context)
